7950ea2a3af0e7a382cc4eacfc4640d0d6fe45ef
Kaustubh Jadhav <jadhav.kaustubh@gmail.com>
2020-08-19 23:02:30 +0530

    update action to login to ec2

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -8,3 +8,10 @@ mkdir /keys
 
 cd /keys
 
+ssh-keygen -t rsa -b 4096 -N "" -C "temporary-key" -f ec2temp
+
+aws ec2-instance-connect send-ssh-public-key --instance-id i-068f876f69206b12e --instance-os-user ubuntu --availability-zone us-west-2c --ssh-public-key file://ec2temp.pub
+
+ssh -i "ec2temp" -T ubuntu@ec2-54-70-207-86.us-west-2.compute.amazonaws.com
+
+hostname
